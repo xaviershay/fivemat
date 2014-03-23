3a9c44cead756661a697758191a4f230b14e4f6f
Xavier Shay <xavier@rhnh.net>
2014-03-23 10:43:45 -0700

    Provides an RSpec 3 compatible formatter.
    * Opted to keep this separate rather than try to remove duplication
      with the RSpec 2 formatter. It's just subtlely different enough that
      trying to DRY this up would make the code harder to understand.
      little gain. It would also risk introducing bugs into the old
      formatter. Assuming this code is pretty much stable now and that any
      new features or bug fixes would apply only to this new version.
    * ProgressFormatter is private, so now inherits from BaseTextFormatter.
      (Having a discussion with core team about whether it should be public.)
    * Simplified `pending_fixed?` with new consistent API.
    * Use `fixed_color` rather than removed `blue`.
    * Use accessor on `execution_result` rather than treating it as a hash.

--- lib/fivemat.rb
+++ lib/fivemat.rb
@@ -4,12 +4,18 @@ module Fivemat
   autoload :Cucumber, 'fivemat/cucumber'
   autoload :MiniTest, 'fivemat/minitest/unit'
   autoload :RSpec, 'fivemat/rspec'
+  autoload :RSpec3, 'fivemat/rspec3'
   autoload :Spec, 'fivemat/spec'
 
   def self.new(*args)
     case args.size
     when 0 then MiniTest::Unit
-    when 1 then RSpec
+    when 1 then
+      if ::RSpec::Core::Version::STRING >= '3.0.0'
+        RSpec3
+      else
+        RSpec
+      end
     when 2 then Spec
     when 3 then Cucumber
     else
